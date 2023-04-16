e66a82cfee02a4eac491a6ffae5ec7a1ef712c83
Elmar <Elmarov@yandex.ru>
2023-04-16 21:00:06 +0300

    comit 16.04.23

--- js/index.js
+++ js/index.js
@@ -1,5 +1,78 @@
+(function () {
 
-const screenWidth = window.screen.width;
-const screenHeight = window.screen.height;
+    let burger = document.querySelector('.burger-menu');
+    let mobailNav = document.querySelector('.mobail-nav');
 
-console.log(screenHeight);
+    burger.addEventListener('click', function (e) {
+
+        mobailNav.style.display = 'block';
+
+    })
+
+    mobailNav.addEventListener('click', function (e) {
+
+        mobailNav.style.display = 'none';
+
+    })
+
+
+
+}());
+
+// ================================= отзывы ==================================
+
+let revImg = document.querySelectorAll('.rev__item');
+let revList = document.querySelector('.rev__list');
+
+revImg.forEach(function (item) {
+
+
+
+
+
+    item.addEventListener('click', function (e) {
+
+
+
+        for (let i = 0; i < revImg.length; i++) {
+
+            revImg[i].classList.remove('rev__item--activ');
+
+        }
+
+
+
+        let zi = this.classList.add('rev__item--activ');
+
+        let activ = document.querySelector('.rev__item--activ').dataset.name;
+
+
+
+        fn10(activ);
+
+
+    })
+
+})
+
+function fn10(a) {
+
+    let itemrev = document.querySelectorAll('.item-rev');
+
+    for (let i = 0; i < itemrev.length; i++) {
+        let elem = itemrev[i];
+
+        elem.classList.remove('item-rev--activ');
+
+        if (a === elem.dataset.open) {
+
+            elem.classList.add('item-rev--activ');
+
+        }
+
+    }
+
+}
+
+// =======================================================================================================
+
